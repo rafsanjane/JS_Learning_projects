2808cd60d49cb20bc78fc9763689b09bf1db54d9
rafsanjane <rafsanhoby@gmail.com>
2022-12-09 00:17:48 +0600

    React folder explainations

---
+++ MyApp/hello-world/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Rafsan from './Rafsan.svg';
+import './App.css';
+
+function App() {
+  return (
+    <ul>
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+  );
+}
+
+export default App;
+
